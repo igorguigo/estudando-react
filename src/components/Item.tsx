9ecb91d80fb121afd6888ee96bbf33332a0ab19d
igor <igorgodoy2010@hotmail.com>
2023-08-09 18:53:23 -0300

    main | Criando lista com react, com verificações

---
+++ src/components/Item.tsx
@@ -0,0 +1,37 @@
+type Props = {
+    nome: string,
+    email: string,
+    avatar: string,
+    status: boolean,
+    grade1: number,
+    grade2: number,
+}
+
+export function Item({ nome, email, avatar, status, grade1, grade2 }: Props) {
+  return (
+    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+      <th
+        scope="row"
+        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+      >
+        <div className="flex gap-x-4">
+          <img className="w-12 h-12 rounded-full" src={avatar} alt="" />
+          <div className="text">
+            <span className="block">{nome}</span>
+            <span className="block">{email}</span>
+          </div>
+        </div>
+      </th>
+      <td className="px-6 py-4">
+        {status}
+        <span className="p-4 rounded-xl text-white font-bold bg-green-600">
+            {status == true && 'Ativo'}
+            {status == false && 'Inativo'}
+        </span>
+      </td>
+      <td className="px-6 py-4">{grade1}</td>
+      <td className="px-6 py-4">{grade2}</td>
+      <td className="px-6 py-4">{ ((grade1 + grade2)/2).toFixed(2) }</td>
+    </tr>
+  );
+}
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,6 +1,33 @@
+import { Item } from "./Item";
+import { students } from "@/data/students";
 
 export function Table() {
   return (
-    <div>Table</div>
-  )
+    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+          <th scope="col" className="px-6 py-3">
+            Name
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Status
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Grade 1
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Grade 2 
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Final Grade
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {students.map(student => 
+          <Item key={student.id} nome={student.name} email={student.email} avatar={student.avatar} status={student.active} grade1={student.grade1} grade2={student.grade2} />
+        )}
+      </tbody>
+    </table>
+  );
 }
