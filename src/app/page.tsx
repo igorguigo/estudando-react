4deed9c7f30c472083258960ea5da937fd49c3ea
igor <igorgodoy2010@hotmail.com>
2023-08-19 16:19:52 -0300

    galeria-de-imagens | Fazendo galeria de imagens interativa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+"use client"
+
+import { Img } from "@/components/Img";
 import { Table } from "@/components/Table";
+import { imgList } from "@/data/imgList";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative overflow-x-auto">
-        <Table />
-      </div>
+      <section>
+        <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {imgList.map(item => (
+            <Img photo={item} onClick={() => alert('teste')} />
+          ))}
+        </div>
+      </section>
     </main>
   );
 }
