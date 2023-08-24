f05a911e9055b92637332cc8ac86d9f173718fc9
igor <igorgodoy2010@hotmail.com>
2023-08-24 18:36:02 -0300

    quiz-basico | Criando front-end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,25 @@
 "use client"
 
-import { Img } from "@/components/Img";
-import { Modal } from "@/components/Modal";
-import { Table } from "@/components/Table";
+
 import { imgList } from "@/data/imgList";
 import { useState } from "react";
 
 export default function Home() {
 
-  const [showModal, setShowModal] = useState(false);
-  const [imageModal, setImageModal] = useState('');
-
-  const openModal = (id: number) => {
-    const photo = imgList.find(item => item.id === id);
-    if(photo){
-      setImageModal(photo.url);
-      setShowModal(true);
-    }
-
-  }
-
-  const closeModal = () => {
-    setShowModal(false);
-  }
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 relative">
-      <section>
-        <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {imgList.map(item => (
-            <Img photo={item} onClick={() => openModal(item.id)} />
-          ))}
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="p-6 bg-slate-900 rounded-xl divide-y">
+        <header className="text-lg font-bold mb-3" >Quiz de culinária</header>
+        <div className="">
+          <h1 className="text-2xl font-bold pt-2 mb-4">1 - Qual ingrediente não pode faltar?</h1>
+          <ul className="space-y-3">
+            <li className="w-full p-1 border-solid border-2 border-gray-700 rounded-md">Farinha</li>
+            <li className="w-full p-1 border-solid border-2 border-gray-700 rounded-md">Corante</li>
+            <li className="w-full p-1 border-solid border-2 border-gray-700 rounded-md">Água</li>
+            <li className="w-full p-1 border-solid border-2 border-gray-700 rounded-md">Açúcar</li>
+          </ul>
         </div>
-      </section>
-      {showModal &&
-        <Modal image={imageModal} closeModal={closeModal} />
-      }
+      </div>
     </main>
   );
 }
